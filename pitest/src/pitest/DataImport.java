6df614fef38923e6201eb013e0a7f94bcb8c1d99
Markus <antylwa@hotmail.com>
2020-02-02 16:08:07 +0300

    Initial version.

---
+++ pitest/src/pitest/DataImport.java
@@ -0,0 +1,10 @@
+package pitest;
+
+public class DataImport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ pitest/src/pitest/DataReader.java
@@ -0,0 +1,10 @@
+package pitest;
+
+public class DataReader {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
